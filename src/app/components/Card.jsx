6d5ae1a44fe5d90ecca524449880b57ae29fbe81
ahsan <MuhammadgAhsan>
2024-12-29 01:29:38 +0500

    use clinet side component change

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,15 +1,19 @@
+'use client';
 import Link from "next/link";
 
 const Card = ({ icon, text, smalltext, btntext, phoneNumber }) => {
   const handleCall = () => {
-    
+    // Check if the device is mobile (max-width 767px)
     if (window.matchMedia("(max-width: 767px)").matches) {
+      // Use tel: protocol for mobile
       window.location.href = `tel:${phoneNumber}`;
     } else {
-    const skypeLink = `skype:${phoneNumber}?call`;
-      window.location.href = skypeLink; 
+      // For desktop, try Skype if available
+      const skypeLink = `skype:${phoneNumber}?call`;
+      window.location.href = skypeLink;
     }
   };
+
   return (
     <div className="max-w-sm bg-white rounded-lg shadow-lg p-6 text-center hover:transform hover:translate-y-1 hover:shadow-2xl transition-transform duration-300">
       {/* Icon Section */}
@@ -23,9 +27,9 @@ const Card = ({ icon, text, smalltext, btntext, phoneNumber }) => {
       {/* Service Description */}
       <h5 className="text-md text-gray-600 mb-4">{smalltext}</h5>
 
-       {/* Call Us Button */}
-       <button
-        onClick={()=>handleCall(phoneNumber)}
+      {/* Call Us Button */}
+      <button
+        onClick={handleCall}  // Use handleCall directly
         className="inline-block bg-green-500 text-white py-2 px-4 rounded-md text-lg font-semibold hover:bg-green-600 transition-colors"
       >
         {btntext}
